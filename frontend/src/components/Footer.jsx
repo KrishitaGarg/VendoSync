a90919553486cb4bed813ca11265c99c0cb3f088
KrishitaGarg <krishitagarg@gmail.com>
2025-06-26 23:33:31 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-black bg-opacity-70 text-center py-6 mt-auto">
+    <footer id="contact" className="bg-black bg-opacity-70 text-center py-6 mt-auto">
       <p>&copy; 2025 W-Setu India. All rights reserved.</p>
     </footer>
   );
